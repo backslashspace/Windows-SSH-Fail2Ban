81ee690980c0f56c0960fbb62c81e9562f484efe
backslashspace <86926768+backslashspace@users.noreply.github.com>
2023-08-09 13:50:35 +0200

    changed folder structure

---
+++ SRV.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F2B_SRV
+{
+    internal static class SRV
+    {
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
